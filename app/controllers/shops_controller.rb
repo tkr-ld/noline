6fe53b3c2fb85d0425a412381d4bd5cd7d3afabe
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-26 13:27:48 +0900

    ShopのCRUD機能の完成

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -14,10 +14,23 @@ class ShopsController < ApplicationController
   def create
     shop = Shop.new(shop_params)
     shop.save!
-    redirect_to shops_url, notice: "#{shop.name}を登録しました"
+    redirect_to shop_url(shop), notice: "#{shop.name}を登録しました"
   end
 
   def edit
+    @shop = Shop.find(params[:id])
+  end
+
+  def update
+    shop = Shop.find(params[:id])
+    shop.update!(shop_params)
+    redirect_to shop_url(shop), notice: "#{shop.name}を更新しました"
+  end
+
+  def destroy
+    shop = Shop.find(params[:id])
+    shop.destroy
+    redirect_to shops_url, notice: "#{shop.name}を削除しました"
   end
 
   private
