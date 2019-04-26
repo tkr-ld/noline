24578c40a4b64c49db152e74d3393da0b11d1d34
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-26 14:22:54 +0900

    ユーザーの登録、編集機能実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path, notice: "ユーザー「#{@user.name}を登録しました。」"
+    else
+      render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}を更新しました。」"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}を削除しました。」"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users, except: [:index, :show]
+
   root to: 'shops#index'
   resources :shops
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
