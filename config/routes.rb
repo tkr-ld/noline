3dc0fc800603aed218adeabde44872426da0d62e
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-26 14:49:19 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users, except: [:index, :show]
 
   root to: 'shops#index'
