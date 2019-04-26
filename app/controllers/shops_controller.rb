ae3ea45b90e8a9208d1252782c9f5660379e543c
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-26 11:59:46 +0900

    店登録機能の実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,13 +1,28 @@
 class ShopsController < ApplicationController
   def index
+    @shops = Shop.all
   end
 
   def show
+    @shop = Shop.find(params[:id])
   end
 
   def new
+    @shop = Shop.new
+  end
+
+  def create
+    shop = Shop.new(shop_params)
+    shop.save!
+    redirect_to shops_url, notice: "#{shop.name}を登録しました"
   end
 
   def edit
+  end
+
+  private
+
+  def shop_params
+    params.require(:shop).permit(:name, :place, :wait_time)
   end
 end
