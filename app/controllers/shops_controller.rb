8f9ffa2de72486cd765eb8e0a3351110fdbcbdbf
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-29 12:23:21 +0900

    ナビバーを上部に固定

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,7 +5,7 @@ class ShopsController < ApplicationController
 
   def index
     @q = Shop.where.not(user_id: current_user.id).ransack(params[:q])
-    @shops = @q.result(distinct: true).page(params[:page]).per(4)
+    @shops = @q.result(distinct: true).page(params[:page]).per(12)
   end
 
   def my_shop
