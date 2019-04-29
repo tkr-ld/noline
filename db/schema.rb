d6f73aaf6af85770b68c1789e12fdeeae2637a54
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-29 14:04:36 +0900

    Oauth用カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_125749) do
+ActiveRecord::Schema.define(version: 2019_04_29_050313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,9 @@ ActiveRecord::Schema.define(version: 2019_04_28_125749) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "image_url"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
