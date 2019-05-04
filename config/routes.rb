f819c6ed16ccef766f195884d3836a983b5addc2
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-05-04 13:08:47 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,8 @@ Rails.application.routes.draw do
 
   resources :relationships, only: [:create, :destroy]
 
+  resources :messages, only: [:index]
+
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
